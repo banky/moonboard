27b7d3b6c276fbf05d053ec1bfa2e2f0b6ecd9a1
banky <bankiadebajo1@gmail.com>
2023-03-24 09:56:51 -0400

    Add publish section

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,23 +1,36 @@
 import { DetailedHTMLProps, ButtonHTMLAttributes } from "react";
 import { HoverShadow } from "./hover-shadow";
+import { LoadingSpinner } from "./loading-spinner";
 
 export type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
->;
+> & {
+  loading?: boolean;
+};
 
-export const Button = ({ className = "", ...props }: ButtonProps) => {
+export const Button = ({
+  className = "",
+  loading = false,
+  ...props
+}: ButtonProps) => {
+  const children = loading ? <LoadingSpinner /> : props.children;
+
   return (
     <button
       {...props}
+      disabled={props.disabled || loading}
       className={`bg-primary-brand border-outlines
         border-2 text-white font-bold p-2 px-4 rounded-md
         enabled:hover:bg-accent enabled:hover:text-text-standard 
         enabled:hover:drop-shadow-[5px_5px_0_rgba(30,30,30,1)]
-        enabled:fill-outlines
+        enabled:fill-outlines disabled:bg-transparent
         disabled:text-text-low-contrast disabled:border-text-low-contrast
         disabled:fill-text-low-contrast
+        min-w-[150px]
         ${className}`}
-    />
+    >
+      {children}
+    </button>
   );
 };
--- app/pages/create-moonboard.tsx
+++ app/pages/create-moonboard.tsx
@@ -1,16 +1,39 @@
+import { Button } from "components/button";
 import { Checkbox } from "components/checkbox";
 import { IconButton } from "components/close-button";
+import { NavigationButton } from "components/navigation-button";
+import { useRouter } from "next/router";
+import { NFTStorage } from "nft.storage";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { Plus } from "svg/plus";
 
 const fileTypes = ["JPG", "PNG", "GIF"];
 
+const nftStorageClient = new NFTStorage({
+  token: process.env.NEXT_PUBLIC_NFT_STORAGE_TOKEN ?? "",
+});
+
 export default function CreateMoonboard() {
   const [files, setFiles] = useState<File[]>([]);
+
+  const [pageState, setPageState] = useState<"upload" | "publish">("upload");
+
+  return pageState === "upload" ? (
+    <Upload files={files} setFiles={setFiles} setPageState={setPageState} />
+  ) : (
+    <Publish files={files} setPageState={setPageState} />
+  );
+}
+
+type UploadProps = {
+  files: File[];
+  setFiles: (files: File[]) => void;
+  setPageState: (pageState: "upload" | "publish") => void;
+};
+const Upload = ({ files, setFiles, setPageState }: UploadProps) => {
   const [myPictures, setMyPictures] = useState(false);
   const [communityGuidelines, setCommunityGuidelines] = useState(false);
-
   const submitEnabled = myPictures && communityGuidelines && files.length > 0;
 
   const handleChange = (files: FileList) => {
@@ -20,6 +43,19 @@ export default function CreateMoonboard() {
   return (
     <main>
       <h1 className="m-12 text-center">Create Moonboard</h1>
+
+      <div className="flex justify-between my-8">
+        <NavigationButton href="/">Back</NavigationButton>
+
+        <Button
+          onClick={() => {
+            setPageState("publish");
+          }}
+          disabled={!submitEnabled}
+        >
+          Continue
+        </Button>
+      </div>
 
       <div className="border-2 border-outlines rounded-md max-w-2xl mx-auto">
         <div className="m-4">
@@ -54,15 +90,9 @@ export default function CreateMoonboard() {
               </div>
             </FileUploader>
           </div>
-
-          <div>
-            <IconButton disabled={!submitEnabled}>
-              <Plus />
-            </IconButton>
-          </div>
         </div>
 
-        <div className="w-3/4 mx-auto">
+        <div className="w-3/4 mx-auto mb-8">
           <Checkbox
             id="my-pictures"
             checked={myPictures}
@@ -79,4 +109,43 @@ export default function CreateMoonboard() {
       </div>
     </main>
   );
-}
+};
+
+type PublishProps = {
+  files: File[];
+  setPageState: (pageState: "upload" | "publish") => void;
+};
+const Publish = ({ files, setPageState }: PublishProps) => {
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    const file = files[0];
+
+    const metadata = await nftStorageClient.store({
+      name: "test moonpin 2",
+      description: "This is a test",
+      image: file,
+    });
+
+    console.log("metadata", metadata);
+
+    setLoading(false);
+
+    // router.push("/publish-moonboard");
+  };
+
+  return (
+    <main>
+      <h1 className="m-12 text-center">Publish Moonboard</h1>
+      <div className="flex justify-between my-8">
+        <Button onClick={() => setPageState("upload")}>Back</Button>
+
+        <Button onClick={onSubmit} loading={loading}>
+          Publish Moonboard
+        </Button>
+      </div>
+    </main>
+  );
+};
