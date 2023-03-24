9ad6c67e4f4942e7c893dd35c7e2c3e03f3d20b7
banky <bankiadebajo1@gmail.com>
2023-03-23 20:07:05 -0400

    Add ugly checkbox

--- app/pages/create-moonboard.tsx
+++ app/pages/create-moonboard.tsx
@@ -56,6 +56,32 @@ export default function CreateMoonboard() {
             </IconButton>
           </div>
         </div>
+
+        <div className="w-3/4 mx-auto">
+          <input
+            id="my-pictures"
+            type="checkbox"
+            value=""
+            className="appearance-none w-4 h-4 text-blue-600 bg-gray-100 border-2 border-outlines rounded 
+            focus:ring-blue-500 focus:ring-2 checked:bg-slate-200
+            hover:drop-shadow-[5px_5px_0_rgba(30,30,30,1)]"
+          />
+          <label htmlFor="my-pictures" className="mx-4">
+            These pictures are mine
+          </label>
+          <br />
+          <input
+            id="community-guidelines"
+            type="checkbox"
+            value=""
+            className="appearance-none w-4 h-4 text-blue-600 bg-gray-100 border-2 border-outlines rounded 
+            focus:ring-blue-500 focus:ring-2 checked:bg-slate-200
+            hover:drop-shadow-[5px_5px_0_rgba(30,30,30,1)]"
+          />
+          <label htmlFor="community-guidelines" className="mx-4">
+            I have read community guidelines
+          </label>
+        </div>
       </div>
     </main>
   );
