572ff466da5869aa45c51b697b2f33f93c999c99
banky <bankiadebajo1@gmail.com>
2023-03-26 10:02:27 -0400

    Add optimism and goerli

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,33 +1,27 @@
 import "@reach/dialog/styles.css";
 import "styles/globals.css";
 import { WagmiConfig, createClient, Chain } from "wagmi";
-import {
-  ConnectKitProvider,
-  ConnectKitButton,
-  getDefaultClient,
-} from "connectkit";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
 import { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { Header } from "components/header";
 import {
-  mainnet,
   hardhat,
-  goerli,
-  polygon,
   polygonZkEvmTestnet,
+  optimismGoerli,
+  gnosisChiado,
 } from "wagmi/chains";
 import { Montserrat } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { scrollAlpha } from "constants/custom-chains";
 
-const alchemyId = process.env.ALCHEMY_ID;
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 const getChains = (): Chain[] => {
   if (process.env.NEXT_PUBLIC_ENVIRONMENT === "localhost") {
     return [hardhat];
   } else {
-    return [scrollAlpha, polygonZkEvmTestnet];
+    return [scrollAlpha, polygonZkEvmTestnet, optimismGoerli, gnosisChiado];
   }
 };
 
